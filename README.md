0de81ffaa30c0451669e8d350bc482981d934bab
MonoMoney <contact@monomoney.net>
2023-08-31 13:34:14 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# @monoswapnet/default-token-list
+
+## Adding a token
+
+Open a new Issue or contact listing@monoswap.net
+
+required information:
+
+Token Name and Symbol
+
+Token Contract Address
+
+Project's Website
+
+Coinmarketcap link
+
+Please note that filing an issue does not guarantee addition to the Monoswap default token list and token listing is not required to use the Monoswap Interface with a token.
---
+++ src/buildList.js
@@ -0,0 +1,27 @@
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+const testnet = require("./tokens/testnet.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "Monoswap Default List",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "https://monoswap.net/logos/",
+    keywords: ["monoswap", "default"],
+    tokens: [...mainnet, ...testnet]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
